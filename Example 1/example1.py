3d758f93f07a1a6cc328cd3938560a92767f4a83
darshiniparekh <157153709+darshiniparekh@users.noreply.github.com>
2026-04-01 00:24:40 -0700

    Update example1.py

--- Example 1/example1.py
+++ Example 1/example1.py
@@ -1,9 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[7]:
-
-
 import pandas as pd
 import numpy as np
 import scipy.stats as stats
@@ -11,17 +5,10 @@ import statsmodels.api as sm
 from statsmodels.formula.api import ols
 from statsmodels.stats.multicomp import pairwise_tukeyhsd
 import matplotlib.pyplot as plt
-
-
-# In[35]:
-
-
 from pathlib import Path
 
 file_path = Path.home() / "Downloads" / "Three Substrate Sterilization.csv"
 data = pd.read_csv(file_path)
-
-
 data.columns = data.columns.str.strip()
 
 data = data.rename(columns={"Sterilization Method": "sterilization_method"
@@ -43,27 +30,13 @@ print(data.groupby("sterilization_method").size())
 
 print(data.columns.tolist())
 
-
-# In[36]:
-
-
 for name, group in data.groupby("sterilization_method"):
     stat, p = stats.shapiro(group["mushroom_production"])
     print(f"{name}: W={stat:.3f}, p-value={p:.4f}")
 
-
-
-# In[37]:
-
-
 sm.qqplot(data["mushroom_production"], line='s')
 plt.title("Q–Q Plot of Mushroom Production")
 plt.show()
-
-
-
-# In[38]:
-
 
 groups = [
     group["mushroom_production"].values
@@ -73,18 +46,10 @@ groups = [
 stat, p = stats.levene(*groups)
 print(f"Levene’s test: W={stat:.3f}, p-value={p:.4f}")
 
-
-# In[39]:
-
-
 model = ols("mushroom_production ~ C(sterilization_method)",data=data).fit()
 
 anova_table = sm.stats.anova_lm(model, typ=2)
 print(anova_table)
-
-
-# In[40]:
-
 
 tukey = pairwise_tukeyhsd(
     endog=data["mushroom_production"],
@@ -93,10 +58,6 @@ tukey = pairwise_tukeyhsd(
 )
 
 print(tukey.summary())
-
-
-# In[41]:
-
 
 data.boxplot(
     column="mushroom_production",
